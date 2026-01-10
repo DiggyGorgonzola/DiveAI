875de2cbd5877b20514d51213d564cc9ce409493
Diggy Gorgonzola <164104182+DiggyGorgonzola@users.noreply.github.com>
2026-01-09 18:47:02 -0600

    Update AIclass.py

--- AIclass.py
+++ AIclass.py
@@ -19,13 +19,30 @@ class AI():
         return key
     return None
   def step(self):
-    DIVEL, DIVER, DIVEU, DIVED = DIVE(grid=self.dive.grid, score=self.dive.score, seeds=self.dive.seeds, changed=self.dive.changed),DIVE(grid=self.dive.grid, score=self.dive.score, seeds=self.dive.seeds, changed=self.dive.changed),DIVE(grid=self.dive.grid, score=self.dive.score, seeds=self.dive.seeds, changed=self.dive.changed),DIVE(grid=self.dive.grid, score=self.dive.score, seeds=self.dive.seeds, changed=self.dive.changed)
-    a = {"l":AI.score_position(DIVEL.move("l"), self.weights),
-         "r":AI.score_position(DIVER.move("r"), self.weights),
-         "u":AI.score_position(DIVEU.move("u"), self.weights),
-         "d":AI.score_position(DIVED.move("d"), self.weights)}
+    DIVELL, DIVELR, DIVELU, DIVELD, DIVERL, DIVERR, DIVERU, DIVERD, DIVEUL, DIVEUR, DIVEUU, DIVEUD, DIVEDL, DIVEDR, DIVEDU, DIVEDD = (DIVE(grid=self.dive.grid, score=self.dive.score, seeds=self.dive.seeds, changed=self.dive.changed),)*16
+    a = {"ll":AI.score_position(DIVELL.move("l").move("l"), self.weights),
+         "lr":AI.score_position(DIVELR.move("l").move("r"), self.weights),
+         "lu":AI.score_position(DIVELU.move("l").move("u"), self.weights),
+         "ld":AI.score_position(DIVELD.move("l").move("d"), self.weights),
+
+         "rl":AI.score_position(DIVERL.move("r").move("l"), self.weights),
+         "rr":AI.score_position(DIVERR.move("r").move("r"), self.weights),
+         "ru":AI.score_position(DIVERU.move("r").move("u"), self.weights),
+         "rd":AI.score_position(DIVERD.move("r").move("d"), self.weights),
+
+         "ul":AI.score_position(DIVEUL.move("u").move("l"), self.weights),
+         "ur":AI.score_position(DIVEUR.move("u").move("r"), self.weights),
+         "uu":AI.score_position(DIVEUU.move("u").move("u"), self.weights),
+         "ud":AI.score_position(DIVEUD.move("u").move("d"), self.weights),
+
+         "dl":AI.score_position(DIVEDL.move("d").move("l"), self.weights),
+         "dr":AI.score_position(DIVEDR.move("d").move("r"), self.weights),
+         "du":AI.score_position(DIVEDU.move("d").move("u"), self.weights),
+         "dd":AI.score_position(DIVEDD.move("d").move("d"), self.weights)}
         #Get the best move
-    self.dive = self.dive.move(AI.get_key(a, a[max(a, key=a.get)]))
+    self.dive = self.dive.move(AI.get_key(a, a[max(a, key=a.get)])[0])
+    self.dive.grid = DIVE.add_new_tile(self.dive.grid, random.choice(self.dive.seeds))
+    self.dive = self.dive.move(AI.get_key(a, a[max(a, key=a.get)])[1])
     self.dive.grid = DIVE.add_new_tile(self.dive.grid, random.choice(self.dive.seeds))
     return self.dive
   def give_birth(self, juvenoia=1):
@@ -33,7 +50,7 @@ class AI():
     for i in range(len(self.weights)):
       child_weights[i] += juvenoia*(random.random()-1/2)
       
-    return AI(weights=child_weights, name=self.name+f".child{100*round(random.random(),3)}", parent=self, dive=DIVE.NEW())
+    return AI(weights=child_weights, name=self.name+f".child{round(1000*random.random(),3)}", parent=self, dive=DIVE.NEW())
   def run_till_death(self):
     while len(DIVE.get_zeros(self.dive.grid)) > 0:
       self.step()
@@ -54,20 +71,29 @@ class TheTormenter():
     winner = AI.get_key(self.g_first_scores,self.g_first_scores[max(self.g_first_scores,key=self.g_first_scores.get)])
     self.g_next.append(winner)
     for i in range(len(self.g_first)-1):
-      self.g_next.append(winner.give_birth())
+      self.g_next.append(winner.give_birth(i))
     self.g_first = self.g_next
     self.g_next = []
     self.g_first_scores = {}
     return winner
+  
+  def TORTURE(self, base, target):
+    winner = base
+    while base.dive.score < target:
+      try:
+        base.dive = DIVE.NEW()
+        print(TT.TORMENT())
+        winner = TT.POPULATE()
+        print("WINNER:",winner.name,"\nWEIGHTS:",winner.weights, "\nSCORE:",winner.dive.score)
+        base = winner
+      except:
+        pass
+    return winner
+
 
 '''-=- testing zone -=- :3'''
 
-guh = AI([100,50,-50,10,20], parent=None, dive=DIVE.NEW())
+guh = AI([76.87868355314122, 24.2506177131773, -148.91126384565317, 63.32513101152392, -7.648193259774409], parent=None, dive=DIVE.NEW())
 TT = TheTormenter([guh, guh.give_birth(), guh.give_birth(), guh.give_birth(), guh.give_birth(), guh.give_birth(), guh.give_birth()])
 while True:
-  print(TT.TORMENT())
-  winner = TT.POPULATE()
-  print("WINNER:",winner.name,"\nWEIGHTS:",winner.weights "\nSCORE:",winner.score)
-  guh = winner
-  TT = TheTormenter(TT.g_next)
-  time.sleep(1)
+  TT.TORTURE(guh,1000)
