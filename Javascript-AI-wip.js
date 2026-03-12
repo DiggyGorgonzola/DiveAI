f2642fe7b6ce05ebbaab9c3355f0f6ba76e19ffe
Diggy Gorgonzola <164104182+DiggyGorgonzola@users.noreply.github.com>
2026-03-12 13:41:21 -0500

    Update Javascript-AI-wip.js

--- Javascript-AI-wip.js
+++ Javascript-AI-wip.js
@@ -1,42 +1,39 @@
 // ==UserScript==
-// @name         New Userscript
+// @name         DIVE AI
 // @namespace    http://tampermonkey.net/
 // @version      2026-03-10
-// @description  try to take over the world!
-// @author       Diggy Gorgonzola
+// @description  a simple AI for the game Dive
+// @author       Gracie 417
 // @match        https://alexfink.github.io/dive/
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=github.io
 // @grant        none
 // ==/UserScript==
 
-window.game_copies = []
-
 window.randomName = function () {
     const f_name = ["Tsa", "Diggy", "ThaAwesome", "Stopside", "Noob", "Nebula", "Gingy", "Amber", "Hephren", "Allu", "Catzee", "Tacocatergy", "NatNat", "Nara"]
     return f_name[Math.floor(Math.random()*f_name.length)].concat(Math.floor(Math.random()*1000))
 };
-window.copyGame = function (copy, name = null) {
-    var gamer = new GameManager2(4, KeyboardInputManager, HTMLActuator, LocalScoreManager, copy, name);
-    window.game_copies.push(gamer);
+window.copyGame = function (copy, name = null, path_add = null) {
+    var gamer = new GameManager2(4, KeyboardInputManager, HTMLActuator, LocalScoreManager, copy, name, path_add);
     return gamer;
 };
 window.copyCurrentGame = function () {
-    return window.copyGame(window.game);
+    return window.copyGame(window.game, null, null);
 };
 window.killGameCopy = function (num) {
     window.game_copies.splice(num, 1);
 }
 
 window.deep = function (copy) {
-    copy = window.copyGame(copy);
+    copy = window.copyGame(copy, copy.name, copy.path);
     var z = [
-        {"GAME":window.copyGame(copy, copy.name.concat("-U")), "MOVE":"0"},
-        {"GAME":window.copyGame(copy, copy.name.concat("-R")), "MOVE":"1"},
-        {"GAME":window.copyGame(copy, copy.name.concat("-D")), "MOVE":"2"},
-        {"GAME":window.copyGame(copy, copy.name.concat("-L")), "MOVE":"3"},
+        window.copyGame(copy, copy.name.concat("-U"), "0"),
+        window.copyGame(copy, copy.name.concat("-R"), "1"),
+        window.copyGame(copy, copy.name.concat("-D"), "2"),
+        window.copyGame(copy, copy.name.concat("-L"), "3")
         ]
     for (var J = 0; J < z.length; J++) {
-        z[J].GAME.move(J)
+        z[J].move(J)
     };
     return z
 };
@@ -45,30 +42,227 @@ window.multi_deep = function (depth, copy) {
 };
 
 window.bestMove = function (depth) {
-    var v = window.multi_deep(depth, window.copyCurrentGame())
-    console.log(v[0])
-    var bs = v[0].GAME.evaluation
-    var bs_move = v[0].MOVE
-    for (var i = 0; i < v.length; i++) {
-        if (v[i].GAME.evaluation > bs) {
-            bs = v[i].GAME.evaluation;
-            bs_move = v[i].MOVE;
+    var v = [window.copyCurrentGame()]
+    var v2 = []
+    for (var x = 0; x < depth; x++) {
+        v2 = []
+        for (var i = 0; i < v.length; i++) {
+            var q = window.deep(v[i])
+            for (var y = 0; y < q.length; y++) {
+                v2.push(q[y])
+            }
+        }
+        v = []
+        for (var z = 0; z < v2.length; z++) {
+            v.push(v2[z])
+        }
+    }
+
+    while (v.length > 0) {
+        var bs = v[0].evaluation
+        var bs_move = v[0]
+        for (var i = 0; i < v.length; i++) {
+            if (v[i].evaluation > bs) {
+                bs = v[i].evaluation;
+                bs_move = v[i];
+            }
+        };
+        q = window.copyCurrentGame()
+        q.move(Number(bs_move.path[0]));
+        if (q.moved) {
+            return bs_move;
+        } else {
+            v.indexOf(bs_move) != -1 ? v.splice(v.indexOf(bs_move), 1) : console.log("CRITICAL FAILURE 2")
         };
     };
-
-    console.log(bs_move);
-    return Number(bs_move[0]);
-}
-
-window.RunAI = function (times) {
-    for (var i = 0; i < times; i++) {
-        window.game.move(window.bestMove());
+    console.log("CRITICAL FAILURE")
+};
+window.doBestMove = function (depth) {
+    if (!window.game.over) {
+    var v = window.bestMove(depth)
+    //console.log("Trying to move to ".concat(v.name))
+    console.log("Expected reward: ".concat(v.evaluation).concat(" cuddles"))
+    window.game.move(Number(v.path[0]));
     }
 };
+
+window.Run = function (times, delay, search_depth) {
+    let count = 0;
+
+    const interval = setInterval(() => {
+        window.doBestMove(search_depth);
+        count++;
+        if (times) {
+            if (count >= times) {
+                clearInterval(interval);
+            };
+        };
+        if (window.game.over) {
+            clearInterval(interval);
+        };
+    }, delay);
+};
+
 window.move = function (dir) {
     window.game.move(dir);
 };
 
+window.copyGrid = function(grid) {
+    var a = new Grid(4)
+    for (var x = 0; x < grid.cells.length; x++) {
+        for (var y = 0; y < grid.cells.length; y++) {
+            if (grid.cells[x][y] != null) {
+                var tile = new Tile({x:x,y:y},grid.cells[x][y].value)
+                a.insertTile(tile)
+            }
+        }
+    }
+    return a
+}
+
+window.thingy = function (copy) {
+  var v = window.largestTile(copy.grid)
+  var row = v[0]
+  var col = v[1]
+  return window.adjacentZeros(row,col)
+}
+window.adjacentZeros = function (row, col, copy) {
+  const directions = [
+    [-1, 0], // up
+    [1, 0],  // down
+    [0, -1], // left
+    [0, 1]   // right
+  ];
+
+  let count = 0;
+
+  for (const [dr, dc] of directions) {
+    const r = row + dr;
+    const c = col + dc;
+
+    if (r >= 0 && r < 4 && c >= 0 && c < 4) {
+      if (copy.grid.cells[r][c] === null) {
+        count++;
+      }
+    }
+  }
+
+  return count;
+}
+window.largestTile = function (grid) {
+    var v = 0
+    var pos = [0,0]
+    for (var x = 0; x < grid.cells.length; x++) {
+        for (var y = 0; y < grid.cells[x].length; y++) {
+            if (grid.cells[x][y] !== null && grid.cells[x][y].value > v) {
+                v = grid.cells[x][y].value
+                pos = [x,y]
+            };
+        };
+    };
+    return pos
+};
+
+window.seedFactors = function (tile, seeds) {
+    var l = 0
+    for (var x = 0; x < seeds.length; x++) {
+        (tile.value % seeds[x] == 0) ? l++ : l += 0
+    }
+    return l
+};
+window.primeFactors = function (value) {
+    var l = []
+    for (var x = 2; x <= value; x++) {
+        if (window.isPrime(x) && value % x === 0) {
+            l.push(x)
+        }
+    }
+    return l;
+};
+function gcd(a, b) {
+  a = Math.abs(a);
+  b = Math.abs(b);
+
+  while (b !== 0) {
+    let temp = b;
+    b = a % b;
+    a = temp;
+  }
+  return a;
+}
+
+window.merges = function (tile1, tile2) {
+    return gcd(tile1.value, tile2.value) == tile1.value || gcd(tile1.value, tile2.value) == tile2.value
+};
+
+window.numMerges = function (copy, row, col) {
+    var merges = 0
+    let rt, ct
+
+    // down
+    rt = row + 1; ct = col;
+    while (rt < 4 && copy.grid.cells[rt][ct] === null) rt++
+    if (rt < 4 && window.merges(copy.grid.cells[row][col], copy.grid.cells[rt][ct])) merges++
+
+    // up
+    rt = row - 1; ct = col;
+    while (rt >= 0 && copy.grid.cells[rt][ct] === null) rt--
+    if (rt >= 0 && window.merges(copy.grid.cells[row][col], copy.grid.cells[rt][ct])) merges++
+
+    // right
+    rt = row; ct = col + 1;
+    while (ct < 4 && copy.grid.cells[rt][ct] === null) ct++
+    if (ct < 4 && window.merges(copy.grid.cells[row][col], copy.grid.cells[rt][ct])) merges++
+
+    // left
+    rt = row; ct = col - 1;
+    while (ct >= 0 && copy.grid.cells[rt][ct] === null) ct--
+    if (ct >= 0 && window.merges(copy.grid.cells[row][col], copy.grid.cells[rt][ct])) merges++
+
+    return merges
+};
+window.isPrime = function (number) {
+    var prime = true;
+    for (var x = 2; x < Math.sqrt(number); x += 2) {
+        if (number % x === 0) {
+            prime = false;
+        };
+    };
+    return prime;
+};
+
+window.isSeed = function (number, seeds) {
+    var seed = true;
+    for (var val of seeds) {
+        if (number % val === 0) {
+            seed = false;
+        };
+    };
+    return seed;
+};
+
+window.seedEvaluation = function (board) {
+    const seeds = board.tileTypes;
+    var incr = 0;
+    for (var i = 0; i < seeds.length; i++) {
+        var seed = seeds[i]
+        if (seed === 2) {
+            incr += 200
+        };
+        if (seed % 6 === 5) {
+            incr += 20
+        };
+        for (var a = 1; a < 6; a++) {
+            for (var b = 1; b < 6; b++) {
+                if (window.isSeed(a*seed + b, seeds)) {
+                    incr += 50
+                };
+            };
+        };
+        incr += window.primeFactors(seed).reduce((a,v)=>a+v,0) / (window.primeFactors(seed).length+1)
+    };
+    return incr
+};
 
 
 
@@ -77,21 +271,20 @@ window.move = function (dir) {
 
 
 
-
-
-
-function GameManager2(size, InputManager, Actuator, ScoreManager, copy_from, name) {
+function GameManager2(size, InputManager, Actuator, ScoreManager, copy_from, name, path_add = null) {
     this.size         = size; // Size of the grid
     this.name         = (name === null) ? window.randomName() : name;
     this.inputManager = new InputManager;
     this.scoreManager = new ScoreManager;
     this.actuator     = new Actuator;
 
-    this.grid         = new Grid(this.size);
+    this.grid         = window.copyGrid(copy_from.grid);
     this.score        = copy_from.score;
     this.tileTypes    = copy_from.tileTypes;
+    this.moved        = false;
     this.over         = false;
     this.won          = false;
+    this.path         = (path_add === null) ? "" : copy_from.path.concat(path_add);
 
 
     this.evaluation   = this.evaluate();
@@ -99,22 +292,25 @@ function GameManager2(size, InputManager, Actuator, ScoreManager, copy_from, nam
     // Set game mode for best score
     this.scoreManager.setGameMode(1);
 
-    // Add the initial tiles
-    for (var x = 0; x < copy_from.grid.cells.length; x++) {
-        for (var y = 0; y < copy_from.grid.cells[x].length; y++) {
-            if (copy_from.grid.cells[x][y] != null) {
-                this.grid.insertTile(copy_from.grid.cells[x][y]);
-            };
-        };
-    };
-
 
 };
 GameManager2.prototype.evaluate = function () {
-    var ava_cells = 10 * this.grid.availableCells().length;
-    var scor = this.score;
-    var seeds = -100 * this.tileTypes;
-    return ava_cells + scor + seeds;
+    var evan = 0;
+    evan -= 50 * Math.pow(this.tileTypes.length,2);
+    evan += 10   * Math.max(...this.tileTypes);
+    evan += 100     * window.seedEvaluation(this);
+    for (var x = 0; x < this.grid.cells.length; x++) {
+        for (var y = 0; y < this.grid.cells[x].length; y++) {
+            var tile = this.grid.cells[x][y];
+            if (tile === null) {
+                evan += 20
+            } else {
+                evan -= 1  * (window.seedFactors(tile,this.tileTypes) - 2);
+                evan += 1  * window.adjacentZeros(tile.x,tile.y,this) * Math.log(tile.value);
+            };
+        };
+    };
+    return evan
 };
 // Save all tile positions and remove merger info
 GameManager2.prototype.prepareTiles = function () {
@@ -144,7 +340,7 @@ GameManager2.prototype.move = function (direction) {
 
   var vector     = this.getVector(direction);
   var traversals = this.buildTraversals(vector);
-  var moved      = false;
+  this.moved     = false;
   var newPrimes  = [];
 
   // Save the current tile positions and remove merger information
@@ -193,7 +389,7 @@ GameManager2.prototype.move = function (direction) {
         }
 
         if (!self.positionsEqual(cell, tile)) {
-          moved = true; // The tile moved from its original cell!
+          self.moved = true; // The tile moved from its original cell!
         }
       }
     });
@@ -210,7 +406,7 @@ GameManager2.prototype.move = function (direction) {
     self.tileTypes = self.tileTypes.concat(newPrimes);
   }
 
-  if (moved) {
+  if (this.moved) {
     if ((self.gameMode & 1) && newPrimes.length) {
       // in mode 1, score for unlocking
       if ((self.gameMode & 3) == 1) {
@@ -282,6 +478,7 @@ GameManager2.prototype.move = function (direction) {
 
     //this.actuate();
   } // if (moved)
+  this.evaluation = this.evaluate();
 };
 
 GameManager2.prototype.div = function (next, cur) {
@@ -390,4 +587,177 @@ GameManager2.prototype.positionsEqual = function (first, second) {
   return first.x === second.x && first.y === second.y;
 };
 
+
+
+
+
+
+
+// STUFF FOR REPLAYS
+
+window.replayMove = function (board, direction) {
+    // 0: up, 1: right, 2:down, 3: left
+  var self = board;
+
+  if (board.over || board.won) return; // Don't do anything if the game's over
+
+  var cell, tile;
+
+  var vector     = board.getVector(direction);
+  var traversals = board.buildTraversals(vector);
+  board.moved     = false;
+  var newPrimes  = [];
+
+  // Save the current tile positions and remove merger information
+  board.prepareTiles();
+
+  var ominosityBound;
+  if (self.gameMode && 1) {
+    // The idea of this is that a seed is ominous roughly if it exceeds
+    // the 2i-th prime, if there are i seeds so far.
+    // But add a little more, to be forgiving.
+    ominosityBound = self.tileTypes.length * 2;
+    ominosityBound *= Math.log(ominosityBound);
+    ominosityBound += 8;
+  }
+
+  // Traverse the grid in the right direction and move tiles
+  traversals.x.forEach(function (x) {
+    traversals.y.forEach(function (y) {
+      cell = { x: x, y: y };
+      tile = self.grid.cellContent(cell);
+      if (tile) {
+        var positions = self.findFarthestPosition(cell, vector);
+        var next      = self.grid.cellContent(positions.next);
+
+        // Only one merger per row traversal?
+        if (next && self.div(next.value, tile.value) > 0 && !next.mergedFrom) {
+          var merged = new Tile(positions.next, self.div(next.value, tile.value));
+          merged.mergedFrom = [tile, next];
+
+          self.grid.insertTile(merged);
+          self.grid.removeTile(tile);
+
+          // Converge the two tiles' positions
+          tile.updatePosition(positions.next);
+
+          // Update the score
+          self.score += Math.min(next.value, tile.value);
+
+          // Unlock new primes, if in that mode
+          if (self.gameMode & 1) {
+            var primes = self.extractNewPrimes(merged.value);
+            newPrimes = newPrimes.concat(primes);
+          }
+        } else { // if (tile)
+          self.moveTile(tile, positions.farthest);
+        }
+
+        if (!self.positionsEqual(cell, tile)) {
+          self.moved = true; // The tile moved from its original cell!
+        }
+      }
+    });
+  });
+
+  if (self.gameMode & 1) {
+    // remove duplicates
+    if (newPrimes.length >= 2) {
+      newPrimes.sort(function (a,b){return a-b});
+      for (var i = newPrimes.length - 2; i >= 0; i--)
+        if (newPrimes[i] == newPrimes[i+1])
+          newPrimes.splice(i,1);
+    }
+    self.tileTypes = self.tileTypes.concat(newPrimes);
+  }
+
+  if (this.moved) {
+    if ((self.gameMode & 1) && newPrimes.length) {
+      // in mode 1, score for unlocking
+      if ((self.gameMode & 3) == 1) {
+        self.score += newPrimes.reduce(function(x,y){return x+y});
+      }
+
+      self.tilesSeen.push.apply(self.tilesSeen, newPrimes);
+
+      var verb = " unlocked!";
+      if (newPrimes.filter(function(x){return x > ominosityBound}).length)
+        verb = " unleashed!";
+      var list = String(newPrimes.pop());
+      if (newPrimes.length) {
+        list = newPrimes.join(", ") + " and " + list;
+      }
+      self.actuator.announce(list + verb);
+      self.actuator.updateCurrentlyUnlocked(self.tileTypes);
+    } // mode 1 only
+
+    if ((self.gameMode & 3) == 3) {
+      // Eliminate primes now absent.
+      var eliminatedIndices = [];
+      for (var i = 0; i < self.tileTypes.length; i++)
+        eliminatedIndices.push(i);
+
+      traversals.x.forEach(function (x) {
+        traversals.y.forEach(function (y) {
+          cell = { x: x, y: y };
+          tile = self.grid.cellContent(cell);
+          if (tile) {
+            for(var i = 0; i < self.tileTypes.length; i++) {
+              if (tile.value % self.tileTypes[i] == 0)
+                eliminatedIndices[i] = null;
+            }
+          }
+        });
+      });
+
+      eliminatedIndices = eliminatedIndices.filter(function (x) {return x != null});
+      if (eliminatedIndices.length) {
+        var eliminatedPrimes = eliminatedIndices.map(function (x) {return self.tileTypes[x]});
+        self.score += eliminatedPrimes.reduce(function(x,y){return x+y});
+
+        var verb = " eliminated!"
+        if (eliminatedPrimes.filter(function(x){return x > ominosityBound}).length)
+          verb = " vanquished!";
+        var list = String(eliminatedPrimes.pop());
+        if (eliminatedPrimes.length) {
+          list = eliminatedPrimes.join(", ") + " and " + list;
+        }
+        self.actuator.announce(list + verb);
+        self.actuator.updateCurrentlyUnlocked(self.tileTypes);
+      }
+
+      for(var i = eliminatedIndices.length - 1; i >= 0; i--)
+        self.tileTypes.splice(eliminatedIndices[i],1);
+      self.actuator.updateCurrentlyUnlocked(self.tileTypes);
+    } // mode 3
+
+    if (!this.movesAvailable()) { // Game over!
+      if ((this.gameMode & 3) == 3)
+        this.over = { tileTypes: this.tileTypes,
+                      tilesSeen: this.tilesSeen };
+      else
+        this.over = {};
+    }
+
+    board.actuate();
+  } // if (moved)
+};
+
+window.replay = function (rpstr, delay) {
+    const moves = rpstr.split(";");
+    let count = 0
+
+    const interval = setInterval(() => {
+        let info = moves[count].split(",");
+        let move = Number(info[0]);
+        let tile = new Tile({x:Number(info[1]),y:Number(info[2])},info[3]);
+        console.log(tile)
+        window.replayMove(window.game, move);
+        window.game.actuate();
+        window.game.grid.insertTile(tile);
+        count++
+        if (count >= moves.length-1) {
+            clearInterval(interval)
+        }}, delay);
+};
 
